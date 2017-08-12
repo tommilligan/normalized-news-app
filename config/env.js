86acfe642958a6879f92e6240e2b28c7b956d1b7
Tom Milligan <tom@tommilligan.net>
2017-08-13 00:01:24 +0100

    Added whitelisting of vatiables from .env file

--- config/env.js
+++ config/env.js
@@ -55,11 +55,17 @@ process.env.NODE_PATH = (process.env.NODE_PATH || '')
 
 // Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
 // injected into the application via DefinePlugin in Webpack configuration.
-const REACT_APP = /^REACT_APP_/i;
+
+const whitelistedPatterns = [
+  /^REACT_APP_/i,
+  /^NERVOUS_EFFICIENT_REBEL_/i,
+]
 
 function getClientEnvironment(publicUrl) {
   const raw = Object.keys(process.env)
-    .filter(key => REACT_APP.test(key))
+    .filter(key => {
+      return whitelistedPatterns.some(pattern => pattern.test(key));
+    })
     .reduce(
       (env, key) => {
         env[key] = process.env[key];
