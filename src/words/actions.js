6e8c419b66a782ce3f553fe1c534f88b088e8d99
Tom Milligan <tom@tommilligan.net>
2017-08-15 22:03:40 +0100

    Added types to generators. Check wordUpdate payload is truthy before calling api

--- src/words/actions.js
+++ src/words/actions.js
@@ -1,37 +1,36 @@
 // @flow
 
 export const WORDS_FILTER_NORMALIZE = 'app/words/filter/normalize';
-type WORDS_FILTER_NORMALIZE_TYPE = 'app/words/filter/normalize';
+export type WORDS_FILTER_NORMALIZE_TYPE = 'app/words/filter/normalize';
 export const WORDS_INPUT_UPDATED = 'app/words/input/updated';
-type WORDS_INPUT_UPDATED_TYPE = 'app/words/input/updated';
+export type WORDS_INPUT_UPDATED_TYPE = 'app/words/input/updated';
 export const NORMALIZED_FETCH_SUCCEEDED = 'app/words/normalize/succeeded';
-type NORMALIZED_FETCH_SUCCEEDED_TYPE = 'app/words/normalize/succeeded';
+export type NORMALIZED_FETCH_SUCCEEDED_TYPE = 'app/words/normalize/succeeded';
 export const NORMALIZED_FETCH_FAILED = 'app/words/normalize/failed';
-type NORMALIZED_FETCH_FAILED_TYPE = 'app/words/normalize/failed';
+export type NORMALIZED_FETCH_FAILED_TYPE = 'app/words/normalize/failed';
 
-type WordsFilterNormalizeAction = { type: WORDS_FILTER_NORMALIZE_TYPE };
+export type WordsFilterNormalizeAction = { type: WORDS_FILTER_NORMALIZE_TYPE };
 export function wordsFilterNormalize(): WordsFilterNormalizeAction {
   return {type: WORDS_FILTER_NORMALIZE};
 }
 
-type WordsInputUpdatedAction = { type: WORDS_INPUT_UPDATED_TYPE, data: string };
+export type WordsInputUpdatedAction = { type: WORDS_INPUT_UPDATED_TYPE, data: string };
 export function wordsInputUpdate(data: string): WordsInputUpdatedAction {
   return {type: WORDS_INPUT_UPDATED, data: data};
 }
 
-type NormalizedFetchSucceededAction = { type: NORMALIZED_FETCH_SUCCEEDED_TYPE, data: string };
+export type NormalizedFetchSucceededAction = { type: NORMALIZED_FETCH_SUCCEEDED_TYPE, data: string };
 export function normalizedFetchSucceeded(data: string): NormalizedFetchSucceededAction {
   return {type: NORMALIZED_FETCH_SUCCEEDED, data: data};
 }
 
-type NormalizedFetchFailedAction = { type: NORMALIZED_FETCH_FAILED_TYPE, data: string };
+export type NormalizedFetchFailedAction = { type: NORMALIZED_FETCH_FAILED_TYPE, data: string };
 export function normalizedFetchFailed(data: string): NormalizedFetchFailedAction {
   return {type: NORMALIZED_FETCH_FAILED, data: data};
 }
 
 export type WordAction =
-  | WordsFilterAnonymizeAction
-  | WordsFilterNeutralizeAction
+  | WordsFilterNormalizeAction
   | WordsInputUpdatedAction
   | NormalizedFetchSucceededAction
   | NormalizedFetchFailedAction
--- src/words/normalizeApiSaga.js
+++ src/words/normalizeApiSaga.js
@@ -4,20 +4,23 @@ import { put, call, takeEvery } from 'redux-saga/effects';
 
 import { normalize } from 'normalized-news';
 
+import type { WordsInputUpdatedAction } from './actions';
 import { WORDS_INPUT_UPDATED, normalizedFetchSucceeded, normalizedFetchFailed } from './actions';
 
-function* fetchNormalizedText(action) {
-    console.log("Fetching normalized text");
-    try {
-        const normalizedText = yield call(normalize, action.data);
-        yield put(normalizedFetchSucceeded(normalizedText));
-    } catch (e) {
-        console.error(e);
-        yield put(normalizedFetchFailed(e.message));
+function* fetchNormalizedText(action: WordsInputUpdatedAction): Generator<void, void, string> {
+    if (action.data) {
+        try {
+            console.debug("Fetching normalized text");
+            const normalizedText = yield call(normalize, action.data);
+            yield put(normalizedFetchSucceeded(normalizedText));
+        } catch (e) {
+            console.error(e);
+            yield put(normalizedFetchFailed(e.message));
+        }
     }
 }
 
-export default function* normalizeApi() {
-    console.warn("Starting normalizeApi saga");
+export default function* normalizeApi(): Generator<void, void, void> {
+    console.warn("Starting normalizeApi saga worker");
     yield takeEvery(WORDS_INPUT_UPDATED, fetchNormalizedText);
 }
